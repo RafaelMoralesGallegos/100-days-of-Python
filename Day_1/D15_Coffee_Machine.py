2fadd93d223309f9f0d000c4102afd8a507fd41f
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2024-08-19 11:50:10 -0600

    update Day_1 folder

--- Day_1/D15_Coffee_Machine.py
+++ Day_1/D15_Coffee_Machine.py
@@ -21,7 +21,7 @@ MENU = {
             "coffee": 24,
         },
         "cost": 3.0,
-    }
+    },
 }
 
 profit = 0
@@ -33,39 +33,45 @@ resources = {
 
 turn_off = False
 
+
 def check_resources(orders_ingredients):
-    '''See if ingredients are enough'''
+    """See if ingredients are enough"""
     for item in orders_ingredients:
         if orders_ingredients[item] > resources[item]:
             print(f"Sorry there is not enough {item}.")
             return False
     return True
 
+
 def insert_coins(cost):
-    '''Check if Inserted coins are enough'''
+    """Check if Inserted coins are enough"""
     quartes = int(input("How many quarters?: "))
     dimes = int(input("How many dimes?: "))
     nickles = int(input("How many nickles?: "))
     pennies = int(input("How many pennies?: "))
-    money = quartes*0.25 + dimes*0.10 + nickles*0.05 + pennies*0.01
+    money = quartes * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
     if money > cost:
-        return True, money-cost
+        return True, money - cost
     else:
         return False, money
 
+
 def make_coffee(orders_ingredients):
-    '''Reduce Resources'''
+    """Reduce Resources"""
     global resources
     for item in orders_ingredients:
         resources[item] -= orders_ingredients[item]
 
+
 while not turn_off:
     action = input("What would you like? (espressso/latte/cappuccino): ")
     if action == "report":
-        print(f"Water: {resources['water']}ml\n" \
-              f"Milk: {resources['milk']}ml\n" \
-              f"Coffee: {resources['coffee']}g\n" \
-              f"Money: ${profit}")
+        print(
+            f"Water: {resources['water']}ml\n"
+            f"Milk: {resources['milk']}ml\n"
+            f"Coffee: {resources['coffee']}g\n"
+            f"Money: ${profit}"
+        )
     elif action == "off":
         turn_off = True
     elif action == "espresso" or action == "latte" or action == "cappuccino":
--- Day_1/Day 12.py
+++ Day_1/Day 12.py
@@ -1,5 +1,7 @@
 import random as random
+
 from art import logo
+
 print(logo)
 
 HARD_GUESS = 5
@@ -7,15 +9,17 @@ EASY_GUESS = 10
 NUMBER = random.randrange(0, 101)
 game_over = False
 
+
 def compare_guess(guess):
-  if guess < NUMBER:
-    print("Too low.")
-    return False
-  elif guess > NUMBER:
-    print("Too high.")
-    return False
-  else:
-    return True
+    if guess < NUMBER:
+        print("Too low.")
+        return False
+    elif guess > NUMBER:
+        print("Too high.")
+        return False
+    else:
+        return True
+
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thining of a number between 1 and 100")
@@ -23,22 +27,22 @@ print("I'm thining of a number between 1 and 100")
 dif = input("Choose difficulty. Type 'easy' or 'hard': ")
 
 if dif == "easy":
-  guesses = EASY_GUESS
+    guesses = EASY_GUESS
 elif dif == "hard":
-  guesses = HARD_GUESS
+    guesses = HARD_GUESS
 
 while not game_over:
-  if guesses != 0:
-    print(f"You have {guesses} attempts remaining to guess the number")
-    guess = int(input("Make a guess: "))
-    game_over = compare_guess(guess)
-    guesses -= 1
-    if not game_over and guesses != 0:
-      print("Guess again.")
-  else:
-    game_over = True
+    if guesses != 0:
+        print(f"You have {guesses} attempts remaining to guess the number")
+        guess = int(input("Make a guess: "))
+        game_over = compare_guess(guess)
+        guesses -= 1
+        if not game_over and guesses != 0:
+            print("Guess again.")
+    else:
+        game_over = True
 
 if guess == NUMBER:
-  print(f"You got it! The answer was {NUMBER}")
+    print(f"You got it! The answer was {NUMBER}")
 else:
-  print(f"You ran out of guesses, you lose. The answer was {NUMBER}")
+    print(f"You ran out of guesses, you lose. The answer was {NUMBER}")
