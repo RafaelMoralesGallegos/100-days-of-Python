cc83e0507db0c377558f383cadf1834779d79dbf
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2024-08-19 12:48:55 -0600

    update files in Day_32

--- Day_33/main.py
+++ Day_33/main.py
@@ -18,7 +18,7 @@ window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file=r"Day_33\background.png")
+background_img = PhotoImage(file=r"background.png")
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(
     150,
@@ -30,7 +30,7 @@ quote_text = canvas.create_text(
 )
 canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file=r"Day_33\kanye.png")
+kanye_img = PhotoImage(file=r"kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
