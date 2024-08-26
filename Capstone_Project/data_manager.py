e08514455310859c4ff169172dc57d3b95b124c6
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2024-08-26 17:56:11 -0600

    update DataManager with get_from

--- Capstone_Project/data_manager.py
+++ Capstone_Project/data_manager.py
@@ -1,14 +1,26 @@
 import os
 
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
     def __init__(self) -> None:
         self.api_endpoint = os.environ.get("FLIGHT_DEALS_SHEETY_ENDPOINT")
         self.api_authorize = os.environ.get("FLIGHT_DEALS_SHEETY_AUTH")
+        self.api_headers = {"Authorization": self.api_authorize}
+
+    def get_from(self) -> dict:
+        response = requests.get(url=self.api_endpoint, headers=self.api_headers)
+        sheet_rows = response.json()
+        print(sheet_rows)
+        print(type(sheet_rows))
+        return sheet_rows
 
 
 if __name__ == "__main__":
     excel_sheet = DataManager()
-    print(excel_sheet.api_authorize)
-    print(excel_sheet.api_endpoint)
+    excel_sheet.get_from()
