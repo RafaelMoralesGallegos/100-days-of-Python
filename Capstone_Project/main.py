f2c0d300faefc9afa620603169f3d0bf37c1c5c7
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2024-09-09 15:30:17 -0600

    completed day 39

--- Capstone_Project/main.py
+++ Capstone_Project/main.py
@@ -1,10 +1,18 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 import datetime as dt
+import os
+import smtplib as smtp
 import time
+
+from dotenv import load_dotenv
 
 from data_manager import DataManager
 from flight_data import find_cheapest_flight
 from flight_search import FlightSearch
+
+load_dotenv()
+my_email = os.environ.get("ULTRA_MAIL_MAIL")
+python_mail_password = os.environ.get("ULTRA_MAIL_PASS")
 
 IATA_CODE = "MEX"
 
@@ -21,20 +29,47 @@ def return_lowest_price(data: list):
     return cheepest_flight
 
 
+def check_cheeper_flight(city, flight):
+    if flight.price < city["lowestPrice"]:
+        print(f"{city['city']}: ${flight.price}")
+        send_mail(flight)
+    else:
+        print("Next City")
+
+
+def send_mail(flight):
+    with smtp.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password=python_mail_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg=(
+                f"Subject:Lower Price Alert!\n\n"
+                f"Only ${flight.price} to fly "
+                f"from {flight.origin_airport} "
+                f"to {flight.destination_airport}, "
+                f"on {flight.out_date} "
+                f"until {flight.return_date}"
+            ),
+        )
+    print("email sent")
+
+
 def main():
-    # sheet = DataManager()
-    # sheet_data = sheet.get_from()
-    sheet_data = [
-        {"city": "Paris", "iataCode": "PAR", "lowestPrice": 54, "id": 2},
-        {"city": "Frankfurt", "iataCode": "FRA", "lowestPrice": 42, "id": 3},
-        {"city": "Tokyo", "iataCode": "TYO", "lowestPrice": 485, "id": 4},
-        {"city": "Hong Kong", "iataCode": "HKG", "lowestPrice": 551, "id": 5},
-        {"city": "Istanbul", "iataCode": "IST", "lowestPrice": 95, "id": 6},
-        {"city": "Kuala Lumpur", "iataCode": "KUL", "lowestPrice": 414, "id": 7},
-        {"city": "New York", "iataCode": "NYC", "lowestPrice": 240, "id": 8},
-        {"city": "San Francisco", "iataCode": "SFO", "lowestPrice": 260, "id": 9},
-        {"city": "Dublin", "iataCode": "DBN", "lowestPrice": 378, "id": 10},
-    ]
+    sheet = DataManager()
+    sheet_data = sheet.get_from()
+    # sheet_data = [
+    #     {"city": "Paris", "iataCode": "PAR", "lowestPrice": 1050, "id": 2},
+    #     {"city": "Frankfurt", "iataCode": "FRA", "lowestPrice": 576, "id": 3},
+    #     {"city": "Tokyo", "iataCode": "TYO", "lowestPrice": 780, "id": 4},
+    #     {"city": "Hong Kong", "iataCode": "HKG", "lowestPrice": 650, "id": 5},
+    #     {"city": "Istanbul", "iataCode": "IST", "lowestPrice": 780, "id": 6},
+    #     {"city": "Kuala Lumpur", "iataCode": "KUL", "lowestPrice": 780, "id": 7},
+    #     {"city": "New York", "iataCode": "NYC", "lowestPrice": 200, "id": 8},
+    #     {"city": "San Francisco", "iataCode": "SFO", "lowestPrice": 230, "id": 9},
+    #     {"city": "Dublin", "iataCode": "DBN", "lowestPrice": 576, "id": 10},
+    # ]
     flich_search = FlightSearch()
     sheet_data = flich_search.check_sheet_IATA(sheet_data)
 
@@ -51,7 +86,7 @@ def main():
             flight_list.append(cheapest_flight)
 
         cheapest_flight = return_lowest_price(flight_list)
-        print(f"{city['city']}: £{cheapest_flight.price}")
+        check_cheeper_flight(city, cheapest_flight)
         # Slowing down requests to avoid rate limit
         time.sleep(2)
 
