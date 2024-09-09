d2b2ed7ae87d2306cf1de2e277d2c09898a32d47
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2024-09-09 17:06:08 -0600

    Day 17 excersises

--- Day_17/Day_17.py
+++ Day_17/Day_17.py
@@ -11,4 +11,29 @@ with open(r"file2.txt", "r") as file:
 
 result = [n for n in file1 if n in file2]
 
-print(result)
+# print(result)
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = sentence.split()
+result = {word: len(word) for word in result}
+# print(result)
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+
+def temp_f(temp_c):
+    temp = (temp_c * 9 / 5) + 32
+    return temp
+
+
+weather_f = {day: temp_f(temp) for day, temp in weather_c.items()}
+
+print(weather_f)
