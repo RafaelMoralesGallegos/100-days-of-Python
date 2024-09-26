5995f34a7a9cbe1e99647dfc72918b8d8902801c
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2024-09-26 15:37:32 -0600

    add excerisce 330

--- Day_45/main.py
+++ Day_45/main.py
@@ -8,3 +8,25 @@ soup = BeautifulSoup(website, "html.parser")
 
 # print(soup.title.string)
 # print(soup.prettify())
+# print(soup.p)
+
+all_achor_tags = soup.find_all(name="a")
+for tag in all_achor_tags:
+    # print(tag.getText())
+    # print(tag.get("href"))
+    pass
+
+heading = soup.find(name="h1", id="name")
+# print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading)
+
+company_url = soup.select_one(selector="p a")
+# print(company_url)
+
+name = soup.select(selector="#name")
+# print(name)
+
+headings = soup.select(".heading")
+print(headings)
