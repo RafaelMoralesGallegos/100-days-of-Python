0a1b757ca2c888e18abe24ee8d85193486124990
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2024-10-08 15:38:15 -0600

    add lesson 345 Selenium

--- Day_48/main.py
+++ Day_48/main.py
@@ -1,1 +1,12 @@
 from selenium import webdriver
+
+# Keep Chrome open in browser
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com.mx/")
+
+# To close driver
+# driver.close()
+# driver.quit()
