8e09228f67eb826dea4a083e3b46c6bf4e560619
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-08 08:23:02 -0600

    add new_form to FormBot in main

--- Web_Scraping_Project/main.py
+++ Web_Scraping_Project/main.py
@@ -39,14 +39,14 @@ def get_properties(soup: BeautifulSoup) -> list:
     return property_cards
 
 
-def get_infomation(card: BeautifulSoup) -> tuple:
+def get_infomation(card: BeautifulSoup) -> list:
     address_info = card.find("address").text.strip()  # type: ignore
     new_address = remove_pipe_separator(address_info)
     property_url = card.find("a").get("href")  # type: ignore
     price = card.find("span", {"data-test": "property-card-price"}).text.strip()  # type: ignore
     new_price = remove_after_symbols(price)
 
-    return new_address, property_url, new_price
+    return [new_address, new_price, property_url]
 
 
 def remove_pipe_separator(address_str: str) -> str:
@@ -76,9 +76,7 @@ def remove_after_symbols(price: str) -> str:
 class FromBot:
 
     def __init__(self):
-        chrome_options = Options()
-        chrome_options.add_experimental_option("detach", True)  # Keep the browser open
-
+        """Creation of Form filler bot"""
         # Set Chrome options
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)  # Keep the browser open
@@ -88,6 +86,23 @@ class FromBot:
         self.driver.maximize_window()
         self.wait = WebDriverWait(self.driver, 30)
 
+    def new_form(self, info: list):
+        """Creating a New form of Google Form
+
+        Args:
+            info (list): information of the rent
+        """
+        self.driver.get(SF_RENT_FORM)  # type: ignore
+
+        # Use field locators based on the DOM structure (adjust if necessary)
+        fields = self.driver.find_elements(By.CSS_SELECTOR, "input[type='text']")
+        for i, field in enumerate(fields):
+            field.click()
+            field.send_keys(info[i])
+
+        button = self.driver.find_element(By.CSS_SELECTOR, "div[role='button']")
+        button.click()
+
 
 # Main execution
 def main():
@@ -96,6 +111,9 @@ def main():
     property_info = [get_infomation(card) for card in cards]
 
     bot = FromBot()
+    bot.new_form(property_info[0])
+    # for property in property_info:
+    #     bot.new_form(property)
 
 
 if __name__ == "__main__":
