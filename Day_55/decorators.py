dadc5cfaf04410f7deb2dfcdad649859b34e3bed
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-10 12:50:42 -0600

    update for logging_decorator excersice

--- Day_55/decorators.py
+++ Day_55/decorators.py
@@ -23,5 +23,28 @@ def create_blog_post(user):
 
 
 new_user = User("angela")
-new_user.is_logged_in = True
+# new_user.is_logged_in = True
 create_blog_post(new_user)
+
+## Next section
+# TODO: Create the logging_decorator() function 👇
+
+
+def logging_decorator(function):
+    def wrapper(*args):
+        print(f"You called {function.__name__}{args}")
+        result = function(*args)
+        print(f"It returned: {result}")
+
+    return wrapper
+
+
+# TODO: Use the decorator 👇
+
+
+@logging_decorator
+def a_function(*args):
+    return sum(args)
+
+
+a_function(1, 2, 3)
