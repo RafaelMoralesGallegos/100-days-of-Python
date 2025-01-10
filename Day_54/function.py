3cd279126478c3b462455edfa352c880e44d8c95
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-10 09:14:33 -0600

    finished function file

--- Day_54/function.py
+++ Day_54/function.py
@@ -39,7 +39,42 @@ def outer_function():
     return nested_function
 
 
+# Python Decorator Funciton
+import time
+
+
+def delay_decorator(function):
+    """A decorator function is a function that wraps another function
+    and modifies their funcitonality"""
+
+    def wrapper_function():
+        time.sleep(2)
+        # Do somehting before
+        function()
+        function()
+        # Do somethinng after
+
+    return wrapper_function
+
+
+@delay_decorator  # sytaxis sugar
+def say_hello():
+    print("Hello")
+
+
+@delay_decorator
+def say_bye():
+    print("Bye")
+
+
+def say_greeting():
+    print("How are you?")
+
+
 if __name__ == "__main__":
     # print(f"Result is: {calculate(multiply, 3, 2)}")
-    inner_function = outer_function()
-    inner_function()
+    # inner_function = outer_function()
+    # inner_function()
+    # say_hello()
+    decorated_function = delay_decorator(say_greeting)
+    decorated_function()
