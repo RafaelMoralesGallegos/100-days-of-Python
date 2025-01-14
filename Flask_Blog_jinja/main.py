bf77c19712a4fd7058138b830a814cd74854641d
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-14 17:07:49 -0600

    finish day 57

--- Flask_Blog_jinja/main.py
+++ Flask_Blog_jinja/main.py
@@ -1,25 +1,32 @@
 import requests
 from flask import Flask, render_template
 
+from post import Post
+
+response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+response.raise_for_status()
+data = response.json()
+blog_posts = []
+
+for blog in data:
+    new_blog = Post(blog)
+    blog_posts.append(new_blog)
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    blog_posts = get_blog()
     return render_template("index.html", posts=blog_posts)
 
 
-def get_blog():
-    try:
-        response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
-        response.raise_for_status()
-    except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
-        return False
-    else:
-        data = response.json()
-        return data
+@app.route("/post/<int:id>")
+def get_post(id):
+    request = None
+    for blog in blog_posts:
+        if blog.id == id:
+            request = blog
+    return render_template("post.html", request=request)
 
 
 if __name__ == "__main__":
--- Flask_Blog_jinja/post.py
+++ Flask_Blog_jinja/post.py
@@ -1,2 +1,7 @@
 class Post:
-    pass
+
+    def __init__(self, data: dict):
+        self.id = data["id"]
+        self.title = data["title"]
+        self.subtitle = data["subtitle"]
+        self.body = data["body"]
