98d358f957005f7e4746f4f53fed2cb4ae60e60c
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-14 09:34:37 -0600

    created a todo list

--- Day_56_Jinja_Blog/server.py
+++ Day_56_Jinja_Blog/server.py
@@ -10,8 +10,10 @@ app = Flask(__name__)
 def home():
     random_number = random.randint(1, 10)
     year = date.today().year
+    # *Use kwags functionally to pass variables to html
     return render_template("index.html", number=random_number, year=year)
 
 
+# Todo: continue Udemy section of Jinja
 if __name__ == "__main__":
     app.run(debug=True)
