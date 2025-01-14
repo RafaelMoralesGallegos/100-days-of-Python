8050f2a8f258c5940c0e85ff9660df1057bf44d6
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-14 13:42:29 -0600

    add get_age to server

--- Day_56_Jinja_Blog/server.py
+++ Day_56_Jinja_Blog/server.py
@@ -1,6 +1,7 @@
 import random
 from datetime import date
 
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -14,5 +15,28 @@ def home():
     return render_template("index.html", number=random_number, year=year)
 
 
+def get_age(name: str):
+    """Get the age according to Agify
+
+    Args:
+        name (str): give the name
+
+    Returns:
+        int: number of data if api succesful, False if not
+    """
+    try:
+        parameters = {"name": name}
+        response = requests.get(url="https://api.agify.io?", params=parameters)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error fetching agify data: {e}")
+        return False
+    else:
+        data = response.json()
+        return int(data["age"])
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    name = input("give me name: ")
+    age = get_age(name)
