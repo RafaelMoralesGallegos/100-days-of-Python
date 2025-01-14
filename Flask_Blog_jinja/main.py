7117474ab0f9e135aa033c818eae6c08c82f8083
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-14 16:21:06 -0600

    add get_blog to main

--- Flask_Blog_jinja/main.py
+++ Flask_Blog_jinja/main.py
@@ -1,11 +1,26 @@
+import requests
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template("index.html")
+    blog_posts = get_blog()
+    return render_template("index.html", posts=blog_posts)
+
+
+def get_blog():
+    try:
+        response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return False
+    else:
+        data = response.json()
+        return data
+
 
 if __name__ == "__main__":
     app.run(debug=True)
