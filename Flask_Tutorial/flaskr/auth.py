b86da1f4957ff195f89c0ab2614e916a50daa311
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-14 11:36:21 -0600

    update auth register to register new users

--- Flask_Tutorial/flaskr/auth.py
+++ Flask_Tutorial/flaskr/auth.py
@@ -16,3 +16,35 @@ from flaskr.db import get_db
 
 # New blueprint named auth, that is defined in __name__
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    """Register new users to the app"""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+
+        if error is None:
+            try:
+                # The (?, ?) are a placeholder value
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template("auth/register.html")
