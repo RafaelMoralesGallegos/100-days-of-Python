9bd503317285446f5d7ae9711b9ebe185985c4de
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-16 15:25:06 -0600

    update header and server for url

--- Proj_Web_Blog_Capstone/server.py
+++ Proj_Web_Blog_Capstone/server.py
@@ -8,5 +8,15 @@ def get_home():
     return render_template("index.html")
 
 
+@app.route("/about")
+def get_about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def get_contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
