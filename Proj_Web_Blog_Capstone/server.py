65da967d28c357896816dcc1d6998dd0451f7f13
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-16 15:51:58 -0600

    update blog api to server

--- Proj_Web_Blog_Capstone/server.py
+++ Proj_Web_Blog_Capstone/server.py
@@ -1,21 +1,26 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+response = requests.get(url="https://api.npoint.io/9fd727ea4ce5737d9ced")
+response.raise_for_status()
+data = response.json()
+
 
 @app.route("/")
 def get_home():
-    return render_template("index.html", page="home")
+    return render_template("index.html", blog_posts=data)
 
 
 @app.route("/about")
 def get_about():
-    return render_template("about.html", page="about")
+    return render_template("about.html")
 
 
 @app.route("/contact")
 def get_contact():
-    return render_template("contact.html", page="contact")
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
