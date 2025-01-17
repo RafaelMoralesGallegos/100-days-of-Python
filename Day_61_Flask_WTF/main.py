7a7cd3e411bdd1b2e310196ea2778705ee2e0176
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-17 12:56:31 -0600

    add MyForm to main

--- Day_61_Flask_WTF/main.py
+++ Day_61_Flask_WTF/main.py
@@ -1,4 +1,10 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 """
 Red underlines? Install the required packages first: 
@@ -12,9 +18,10 @@ pip3 install -r requirements.txt
 
 This will install the packages from requirements.txt for this project.
 """
-
-
+load_dotenv()
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ["WTF_CSRF_SECRET_KEY"]
+print(os.environ["WTF_CSRF_SECRET_KEY"])
 
 
 @app.route("/")
@@ -22,9 +29,23 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+# create Flask_WTF class
+class MyForm(FlaskForm):
+    email = StringField("What is your Email?", validators=[DataRequired()])
+    password = StringField("What is your Password?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = MyForm()
+    if form.validate_on_submit():
+        name = form.email.data
+        form.email.data = ""
+        password = form.password.data
+        form.password.data = ""
+
+    return render_template("login.html", form=form)
 
 
 if __name__ == "__main__":
