fc89c98620aed2d5b5229d459fb7b290169c8bf7
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-17 09:49:24 -0600

    update for login and post method

--- html_forms/main.py
+++ html_forms/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 def get_home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def get_login():
+    user_data = {
+        "username": request.form["username"],
+        "password": request.form["password"],
+    }
+    return render_template("login.html", user_data=user_data)
 
 
 if __name__ == "__main__":
