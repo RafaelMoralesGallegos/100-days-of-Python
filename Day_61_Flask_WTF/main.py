6bbd64ae021aa69ea929af446461404dcda32a4e
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-17 12:31:46 -0600

    update main for route to login

--- Day_61_Flask_WTF/main.py
+++ Day_61_Flask_WTF/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-'''
+"""
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
 
@@ -11,7 +11,7 @@ On MacOS type:
 pip3 install -r requirements.txt
 
 This will install the packages from requirements.txt for this project.
-'''
+"""
 
 
 app = Flask(__name__)
@@ -19,8 +19,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
