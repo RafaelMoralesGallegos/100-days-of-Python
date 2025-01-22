c87a6191bfefe5a50381c11b5e3d4337310c3475
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-22 16:17:40 -0600

    update index and add

--- Day_63_SQL/main.py
+++ Day_63_SQL/main.py
@@ -1,33 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 all_books = []
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    pass
+    return render_template("index.html")
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+
+    if request.method == "POST":
+        print(True)
+
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
