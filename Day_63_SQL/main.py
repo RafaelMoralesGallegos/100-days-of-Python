7bc43488160cbd7fbdc404591bb5b705881378ee
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-29 08:49:21 -0600

    add database and table

--- Day_63_SQL/main.py
+++ Day_63_SQL/main.py
@@ -1,21 +1,49 @@
-import sqlite3
-
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Float, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-# cursor.execute(
-#     "CREATE TABLE books (id INTEGER PRIMARY KEY, "
-#     "title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, "
-#     "rating FLOAT NOT NULL)"
-# )
-cursor.execute(
-    "INSERT OR IGNORE INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')"
-)
-db.commit()
 all_books = []
+
+
+##CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# Initialise the app with the extension
+db.init_app(app)
+
+
+##CREATE TABLE
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+
+# CREATE RECORD
+def create_new_book(title, author, rating):
+    with app.app_context():
+        new_book = Book(title=title, author=author, rating=rating)
+        db.session.add(new_book)
+        db.session.commit()
 
 
 @app.route("/")
@@ -27,12 +55,10 @@ def home():
 def add():
 
     if request.method == "POST":
-        all_books.append(
-            {
-                "title": request.form.get("title"),
-                "author": request.form.get("author"),
-                "rating": request.form.get("rating"),
-            }
+        create_new_book(
+            request.form.get("title"),
+            request.form.get("author"),
+            request.form.get("rating"),
         )
         return home()
 
