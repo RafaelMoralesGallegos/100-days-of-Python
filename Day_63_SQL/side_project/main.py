852e9a5c67a6e9d20d51a5ce0a54591d022ba794
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-29 08:29:37 -0600

    udpate for new books in SQL

--- Day_63_SQL/side_project/main.py
+++ Day_63_SQL/side_project/main.py
@@ -36,7 +36,44 @@ with app.app_context():
     db.create_all()
 
 # CREATE RECORD
+# with app.app_context():
+#     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)  # type: ignore
+#     db.session.add(new_book)
+#     db.session.commit()
+
+# READ ALL RECORDS
 with app.app_context():
-    new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)  # type: ignore
-    db.session.add(new_book)
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+
+# READ SINGULAR
+with app.app_context():
+    book = db.session.execute(
+        db.select(Book).where(Book.title == "Harry Potter")
+    ).scalar()
+
+# UPDATE A SINGLE
+# with app.app_context():
+#     boot_to_update = db.session.execute(
+#         db.select(Book).where(Book.title == "Harry Potter")
+#     ).scalar()
+#     boot_to_update.title = "Harry Potter and the Chamber of Secrets"
+#     db.session.commit()
+
+book_id = 1
+with app.app_context():
+    book_to_update = db.session.execute(
+        db.select(Book).where(Book.id == book_id)
+    ).scalar()
+    # or book_to_update = db.get_or_404(Book, book_id)
+    book_to_update.title = "Harry Potter and the Goblet of Fire"
+    db.session.commit()
+
+book_id = 1
+with app.app_context():
+    book_to_delete = db.session.execute(
+        db.select(Book).where(Book.id == book_id)
+    ).scalar()
+    # or book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
     db.session.commit()
