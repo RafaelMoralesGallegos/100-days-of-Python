a09ce8c3ef09789f48bbba41768c66313b8bc87a
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-01-30 09:16:05 -0600

    added Movie db and class for main app

--- Proj_Website_Top/main.py
+++ Proj_Website_Top/main.py
@@ -13,9 +13,28 @@ app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap5(app)
 
 # CREATE DB
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 
 # CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f"<Movie {self.title}>"
+
+
+db.create_all()
 
 
 @app.route("/")
