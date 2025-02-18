c00ecf128f2c6ec2c34b912191633b94e760d86f
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-02-18 16:59:00 -0600

    update files for formatting

--- Day_67_Blog_API/main.py
+++ Day_67_Blog_API/main.py
@@ -1,35 +1,26 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap5
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
-from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
+from flask import Flask, redirect, render_template, url_for
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor, CKEditorField
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from wtforms import StringField, SubmitField
+from wtforms.validators import URL, DataRequired
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap5(app)
+
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -49,14 +40,15 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/')
+@app.route("/")
 def get_all_posts():
     # TODO: Query the database for all the posts. Convert the data to a python list.
     posts = []
     return render_template("index.html", all_posts=posts)
 
+
 # TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route("/")
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
     requested_post = "Grab the post from your database"
@@ -68,6 +60,7 @@ def show_post(post_id):
 # TODO: edit_post() to change an existing blog post
 
 # TODO: delete_post() to remove a blog post from the database
+
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
