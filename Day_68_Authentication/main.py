152b8156362451df0c030dadbe712fb0708a2f42
Rafael Morales Gallegos <rafael.morales@grupodmi.com.mx>
2025-02-19 11:57:35 -0600

    update for formating for easyer writting

--- Day_68_Authentication/main.py
+++ Day_68_Authentication/main.py
@@ -1,27 +1,40 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    url_for,
+)
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config["SECRET_KEY"] = "secret-key-goes-here"
+
 
 # CREATE DATABASE
-
-
 class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+
 # CREATE TABLE IN DB
-
-
 class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
@@ -33,32 +46,32 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route("/register")
 def register():
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route("/login")
 def login():
     return render_template("login.html")
 
 
-@app.route('/secrets')
+@app.route("/secrets")
 def secrets():
     return render_template("secrets.html")
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     pass
 
 
-@app.route('/download')
+@app.route("/download")
 def download():
     pass
 
